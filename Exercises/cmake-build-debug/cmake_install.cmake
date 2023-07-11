e531ef3535b3ba52517f05863ffc7e221992347e
Alfred Yoo <yookanghae@gmail.com>
2023-07-10 19:10:11 -0500

    skeleton

--- Exercises/cmake-build-debug/cmake_install.cmake
+++ Exercises/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/kanghaeyoo/Desktop/ProgrammingC_Notes/Exercises
+# Install script for directory: /Users/kanghaeyoo/Desktop/C-Programming-Language/Exercises
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/kanghaeyoo/Desktop/ProgrammingC_Notes/Exercises/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/kanghaeyoo/Desktop/C-Programming-Language/Exercises/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
